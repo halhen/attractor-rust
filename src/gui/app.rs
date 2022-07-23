26c29fdc5a487679cee04e711504823bf3825955
Henrik Lindberg <henrik@hnlk.se>
2022-07-23 17:39:34 +0200

    Structure as module

---
+++ src/gui/app.rs
@@ -0,0 +1,27 @@
+use eframe::egui;
+
+#[derive(Default, Debug)]
+pub struct App {}
+
+impl App {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Here goes image");
+        });
+    }
+}
+
+pub fn run() {
+  let window_options = eframe::NativeOptions::default();
+  eframe::run_native(
+      "Attractor",
+      window_options,
+      Box::new(|cc| Box::new(App::new(cc))),
+  );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-use eframe::egui;
-
-#[derive(Default, Debug)]
-struct App {}
-
-impl App {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
-    }
-}
-
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Here goes image");
-        });
-    }
-}
+mod gui;
 
 fn main() {
-    let window_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Attractor",
-        window_options,
-        Box::new(|cc| Box::new(App::new(cc))),
-    );
+    gui::app::run();
 }
