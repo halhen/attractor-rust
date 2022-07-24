70443bf16fa9fb1ba3592af63778be69cd7b6bbe
Henrik Lindberg <henrik@hnlk.se>
2022-07-24 10:10:33 +0200

    Make scaling an enum

--- src/attractor/image.rs
+++ src/attractor/image.rs
@@ -1,13 +1,33 @@
+use std::collections::HashMap;
 use super::raster::Raster;
 use image::ImageBuffer;
 
-pub fn render<F>(raster: &Raster, scale: F) ->
-    image::ImageBuffer<image::Rgba<u8>, Vec<u8>>
-    where
-      F: Fn(f64) -> f64
+type ScalingFunction = dyn Fn(f64) -> f64;
+pub enum Scaling {
+  BINARY,
+  LINEAR,
+  SQRT,
+  LOG
+}
+
+fn scaler(scaling: Scaling) -> Box<ScalingFunction> {
+  let function = match scaling {
+    BINARY => |x| if x < 0.001 {0.} else {1.},
+    LINEAR => |x| x,
+    SQRT => |x| f64::sqrt(x),
+    LOG => |x| f64::log2(x + 1.),
+  };
+
+  Box::new(function)
+}
+
+
+pub fn render(raster: &Raster, scaling: Scaling) -> image::ImageBuffer<image::Rgba<u8>, Vec<u8>>
 {
   let width = raster.width() as u32;
   let height = raster.height() as u32;
+
+  let scale = scaler(scaling);
 
   let image = ImageBuffer::from_fn(width, height, |x, y| {
     let intensity = scale(raster.intensity(x, y));
