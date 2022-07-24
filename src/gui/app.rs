1272b16de8f87e933aec86d5d1a08e8a3612833e
Henrik Lindberg <henrik@hnlk.se>
2022-07-24 18:04:02 +0200

    Generate random images

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,18 +1,75 @@
 use eframe::egui;
+use rand::Rng;
+use crate::attractor::{self, quadratic2d};
 
-#[derive(Default, Debug)]
-pub struct App {}
+#[derive(Debug)]
+pub struct App {
+    params: [f64; 12],
+
+    // Cached results
+    swarm: Option<attractor::swarm::Swarm>,
+    raster: Option<attractor::raster::Raster>,
+    image: Option<image::ImageBuffer<image::Rgba<u8>, Vec<u8>>>
+}
 
 impl App {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+        Self {
+            params: [1., 0., -1.4, 0., 0.3, 0., 0., 1., 0., 0., 0., 0.],
+            swarm: None,
+            raster: None,
+            image: None
+        }
+    }
+
+    // 
+    fn refresh_render(&mut self, width: usize, height: usize) {
+         match self.swarm {
+            Some(_) => {},
+            None => self.swarm = Some(attractor::quadratic2d::generate(&self.params, 100_000))
+        };
+
+        match self.raster {
+            Some(_) => {},
+            None => self.raster = Some(attractor::raster::Raster::new(&self.swarm.as_ref().unwrap(), width, height))
+        };
+
+        match self.image {
+            Some(_) => {},
+            None => self.image = Some(attractor::image::render(
+                &self.raster.as_ref().unwrap(),
+                attractor::image::Scaling::LINEAR,
+                colorgrad::greys()
+            ))
+        };
     }
 }
+
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(super::image_widget::ImageWidget::new());
+            
+            let button_response = ui.button("Randomize");
+            if button_response.clicked() {
+                println!("Randomize");
+                self.swarm = None;
+                self.raster = None;
+                self.image = None;
+                let mut rng = rand::thread_rng();
+
+                for i in 0..self.params.len() {
+                    self.params[i] = rng.gen_range(-1.5..=1.5);
+                }
+                println!("{:?}", self.params);
+            }
+
+            self.refresh_render(
+                ui.available_width() as usize,
+                ui.available_height() as usize
+            );
+            
+            ui.add(super::image_widget::ImageWidget::new(&self.image));
         });
     }
 }
--- src/gui/image_widget.rs
+++ src/gui/image_widget.rs
@@ -2,46 +2,45 @@ use eframe::egui;
 use crate::attractor::{quadratic2d::generate, raster::Raster, image::render, image::Scaling};
 use colorgrad;
 
-pub struct ImageWidget {
-  
+pub struct ImageWidget<'a> {
+  image: &'a Option<image::ImageBuffer<image::Rgba<u8>, Vec<u8>>>
 }
 
-impl ImageWidget {
-  pub fn new() -> Self {
-    Self::default()
-  }
-}
-
-impl Default for ImageWidget {
-  fn default() -> Self {
+impl<'a> ImageWidget<'a> {
+  pub fn new(image: &'a Option<image::ImageBuffer<image::Rgba<u8>, Vec<u8>>>) -> Self {
     Self {
+      image: image
     }
   }
 }
 
-impl egui::Widget for ImageWidget {
-  fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-    let size = ui.available_size();
-    let params = [-0.16, -0.37, -0.27, 0.16, -0.66, -0.74, -1.11, -0.51, 0.59, 0.81, -0.06, -0.44];
-    let swarm = generate(&params, 1_000_000);
-    let raster = Raster::new(&swarm, size.x as usize, size.y as usize);
-    let image = render(
-      &raster,
-      Scaling::LINEAR,
-      colorgrad::plasma()
-    );
-
-    let size = [image.width() as _, image.height() as _];
-    let pixels: image::FlatSamples<&[u8]> = image.as_flat_samples();
-    let gui_image = egui::ColorImage::from_rgba_unmultiplied(
-        size,
-        pixels.as_slice()
-    );
-    let mut texture: Option<egui::TextureHandle> = None;
-    let texture: &egui::TextureHandle = texture.get_or_insert_with(|| {
-        ui.ctx().load_texture("attractor-img", gui_image)
-    });
-    let response = ui.image(texture, ui.available_size());
-    response
+impl<'a> Default for ImageWidget<'a> {
+  fn default() -> Self {
+    Self {
+      image: &None
+    }
   }
+}
+
+impl<'a> egui::Widget for ImageWidget<'a> {
+  fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+    match self.image {
+      None => {
+        ui.label("No image yet")
+      },
+      Some(image) => {
+        let size = [image.width() as _, image.height() as _];
+        let pixels: image::FlatSamples<&[u8]> = image.as_flat_samples();
+        let gui_image = egui::ColorImage::from_rgba_unmultiplied(
+            size,
+            pixels.as_slice()
+        );
+        let mut texture: Option<egui::TextureHandle> = None;
+        let texture: &egui::TextureHandle = texture.get_or_insert_with(|| {
+            ui.ctx().load_texture("attractor-img", gui_image)
+        });
+        ui.image(texture, ui.available_size())
+      }
+    }
+  }    
 }
